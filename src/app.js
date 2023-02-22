9e54ca954f1febd158feffe36a0f3a7f74b80c1c
deniskimani <48308527+deniskimani@users.noreply.github.com>
2023-02-22 13:33:21 +0300

    express with js basics all tests working

--- src/app.js
+++ src/app.js
@@ -1,34 +1,48 @@
 /* eslint-disable radix */
 const express = require('express');
-const strings = require('./lib/strings');
-const numbers = require('./lib/numbers');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
+const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
+const { negate, truthiness, isOdd, startsWith } = require('./lib/booleans');
+const {
+  getNthElement,
+  arrayToCSVString,
+  addToArray2,
+  elementsStartingWithAVowel,
+  removeNthElement2,
+} = require('./lib/arrays');
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/strings/hello/:string', (req, res) => {
-  res.json({ result: strings.sayHello(req.params.string) });
+  res.json({ result: sayHello(req.params.string) });
   // res.sendStatus(200);
 });
 
 app.get('/strings/upper/:string', (req, res) => {
-  res.json({ result: strings.uppercase(req.params.string) });
+  res.json({ result: uppercase(req.params.string) });
 });
 
 app.get('/strings/lower/:string', (req, res) => {
-  res.json({ result: strings.lowercase(req.params.string) });
+  res.json({ result: lowercase(req.params.string) });
 });
 
 app.get('/strings/first-character/:string', (req, res) => {
-  res.json({ result: strings.firstCharacter(req.params.string) });
+  res.json({ result: firstCharacter(req.params.string) });
 });
 
 app.get('/strings/first-characters/:string', async (req, res) => {
   const params = req.params.string.toString();
   const query = parseInt(req.query.length);
 
-  res.status(200).json({ result: strings.firstCharacters(params, query) });
+  res.status(200).json({ result: firstCharacters(params, query) });
 });
 
 app.get('/numbers/add/:a/and/:b', (req, res) => {
@@ -36,15 +50,155 @@ app.get('/numbers/add/:a/and/:b', (req, res) => {
   const b = parseInt(req.params.b);
 
   if (Number.isNaN(a) === false || Number.isNaN(b) === false) {
-    res.status(200).json({ result: numbers.add(a, b) });
+    res.status(200).json({ result: add(a, b) });
   } else {
     res.status(400);
-    res.send('Nan values');
+    res.send({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+app.get('/numbers/subtract/:b/from/:a', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+
+  if (Number.isNaN(a) === false || Number.isNaN(b) === false) {
+    res.status(200).json({ result: subtract(a, b) });
+  } else {
+    res.status(400);
+    res.send({ error: 'Parameters must be valid numbers.' });
   }
 });
 
 app.post('/numbers/multiply', (req, res) => {
-  res.status(200).json({ result: numbers.multiply(req.body.a, req.body.b) });
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (Number.isNaN(a) === false || Number.isNaN(b) === false) {
+    res.status(200).json({ result: multiply(a, b) });
+  }
+
+  if (!req.body.b || !req.body.a) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  if (Number.isNaN(a) === true && Number.isNaN(b) === true) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+});
+
+app.post('/numbers/divide', (req, res) => {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (Number.isNaN(a) === false && Number.isNaN(b) === false && b !== 0) {
+    res.status(200).json({ result: divide(a, b) });
+  }
+  if (req.body.b === 0) {
+    res.status(400);
+    res.send({ error: 'Unable to divide by 0.' });
+  }
+  if (!req.body.b || !req.body.a) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  if (Number.isNaN(a) === true && Number.isNaN(b) === true) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+});
+
+app.post('/numbers/remainder', (req, res) => {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (Number.isNaN(a) === false && Number.isNaN(b) === false && b !== 0) {
+    res.status(200).json({ result: remainder(a, b) });
+  }
+  if (req.body.b === 0) {
+    res.status(400);
+    res.send({ error: 'Unable to divide by 0.' });
+  }
+  if (!req.body.b || !req.body.a) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  if (Number.isNaN(a) === true && Number.isNaN(b) === true) {
+    res.status(400);
+    res.send({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+app.post('/booleans/negate', (req, res) => {
+  res.status(200).json({ result: negate(req.body.value) });
+});
+
+app.post('/booleans/truthiness', (req, res) => {
+  res.status(200).json({ result: truthiness(req.body.value) });
+});
+
+app.get('/booleans/is-odd/:n', (req, res) => {
+  const n = parseInt(req.params.n);
+
+  if (Number.isNaN(n) === false) {
+    res.status(200).json({ result: isOdd(n) });
+  } else {
+    res.status(400);
+    res.send({ error: 'Parameter must be a number.' });
+  }
+});
+
+app.get('/booleans/:string/starts-with/:char', (req, res) => {
+  const { string } = req.params;
+  const { char } = req.params;
+
+  if (char.length === 1) {
+    res.status(200).json({ result: startsWith(char, string) });
+  } else {
+    res.status(400);
+    res.send({ error: 'Parameter "character" must be a single character.' });
+  }
+});
+
+app.post('/arrays/element-at-index/:index', (req, res) => {
+  const { array } = req.body;
+  const { index } = req.params;
+
+  res.status(200).json({ result: getNthElement(index, array) });
+});
+
+app.post('/arrays/to-string', (req, res) => {
+  const { array } = req.body;
+
+  res.status(200).json({ result: arrayToCSVString(array) });
+});
+
+app.post('/arrays/append', (req, res) => {
+  const { array } = req.body;
+  const element = req.body.value;
+
+  res.status(200).json({ result: addToArray2(element, array) });
+});
+
+app.post('/arrays/starts-with-vowel', (req, res) => {
+  const { array } = req.body;
+
+  res.status(200).json({ result: elementsStartingWithAVowel(array) });
+});
+
+app.post('/arrays/remove-element', (req, res) => {
+  const { array } = req.body;
+
+  if (req.query === {}) {
+    const givenIndex = 0;
+    res.status(200).json({ result: removeNthElement2(givenIndex, array) });
+  }
+  if (req.query !== {}) {
+    res.status(200).json({ result: removeNthElement2(req.query.index, array) });
+  }
 });
 
 module.exports = app;
--- src/lib/booleans.js
+++ src/lib/booleans.js
@@ -1,167 +1,156 @@
-const { check } = require("prettier")
-
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
 function negate(a) {
-    // your code here
-    return !a
-};
+  // your code here
+  return !a;
+}
 
 function both(a, b) {
-    // your code here
-    if (a && b) {
-        return true
-    }
-    if (a && !b) {
-        return false
-    }
-    if (!a && b) {
-        return false
-    }
-    if (!a && !b) {
-        return false
-    }
-};
+  // your code here
+  if (a && b) {
+    return true;
+  }
+  if (a && !b) {
+    return false;
+  }
+  if (!a && b) {
+    return false;
+  }
+  if (!a && !b) {
+    return false;
+  }
+}
 
 function either(a, b) {
-    // your code here
-    if (a || b) {
-        return true
-    } else {
-        return false
-    }
-
-};
+  // your code here
+  if (a || b) {
+    return true;
+  }
+  return false;
+}
 
 function none(a, b) {
-    // your code here
-    if (!a && !b) {
-        return true
-    }
-    if (a || b) {
-        return false
-    }
-};
+  // your code here
+  if (!a && !b) {
+    return true;
+  }
+  if (a || b) {
+    return false;
+  }
+}
 
 function one(a, b) {
-    // your code here
-    if (a && b) {
-        return false
-    }
-    if (a && !b) {
-        return true
-    }
-    if (!a && b) {
-        return true
-    }
-    if (!a && !b) {
-        return false
-    }
-
-
-};
+  // your code here
+  if (a && b) {
+    return false;
+  }
+  if (a && !b) {
+    return true;
+  }
+  if (!a && b) {
+    return true;
+  }
+  if (!a && !b) {
+    return false;
+  }
+}
 
 function truthiness(a) {
-    // your code here
-    truthy = Boolean(a)
-    return truthy
-};
+  // your code here
+  truthy = Boolean(a);
+  return truthy;
+}
 
 function isEqual(a, b) {
-    // your code here
-    equality = Object.is(a, b)
-    return equality
-};
+  // your code here
+  equality = Object.is(a, b);
+  return equality;
+}
 
 function isGreaterThan(a, b) {
-    // your code here
-    if (a > b) {
-        return true
-    } else {
-        return false
-    }
-};
+  // your code here
+  if (a > b) {
+    return true;
+  }
+  return false;
+}
 
 function isLessThanOrEqualTo(a, b) {
-    // your code here
-    if (a <= b) {
-        return true
-    } else {
-        return false
-    }
-};
+  // your code here
+  if (a <= b) {
+    return true;
+  }
+  return false;
+}
 
 function isOdd(a) {
-    // your code here
-    let checkForOdd = a % 2
-    if (checkForOdd > 0) {
-        return true
-    } else {
-        return false
-    }
-};
+  // your code here
+  const checkForOdd = a % 2;
+  if (checkForOdd > 0) {
+    return true;
+  }
+  return false;
+}
 
 function isEven(a) {
-    // your code here
-    checkForEven = a % 2
-    if (checkForEven === 0) {
-        return true
-    } else {
-        return false
-    }
-};
+  // your code here
+  checkForEven = a % 2;
+  if (checkForEven === 0) {
+    return true;
+  }
+  return false;
+}
 
 function isSquare(a) {
-    // your code here
-    let squareChecker = Math.sqrt(a)
-    let wholeSquare = squareChecker % 1
+  // your code here
+  const squareChecker = Math.sqrt(a);
+  const wholeSquare = squareChecker % 1;
 
-    if (wholeSquare === 0) {
-        return true
-    } else {
-        return false
-    }
-};
+  if (wholeSquare === 0) {
+    return true;
+  }
+  return false;
+}
 
 function startsWith(char, string) {
-    // your code here
-    let firstLetter = string.charAt(0)
-    if (firstLetter === char) {
-        return true
-    } else {
-        return false
-    }
-};
+  // your code here
+  const firstLetter = string.charAt(0);
+  if (firstLetter === char) {
+    return true;
+  }
+  return false;
+}
 
 function containsVowels(string) {
-    // your code here
-    vowels = [string.match(/[aeiou]/ig)];
-    vowelString = vowels.toString()
-    vowelCount = vowelString.length
-    if (vowelCount > 0) {
-        return true
-    } else {
-        return false
-    }
-};
+  // your code here
+  vowels = [string.match(/[aeiou]/gi)];
+  vowelString = vowels.toString();
+  vowelCount = vowelString.length;
+  if (vowelCount > 0) {
+    return true;
+  }
+  return false;
+}
 
 function isLowerCase(string) {
-    // your code here
-    return string === string.toLowerCase() && string != string.toUpperCase();
-};
+  // your code here
+  return string === string.toLowerCase() && string !== string.toUpperCase();
+}
 
 module.exports = {
-    negate,
-    both,
-    either,
-    none,
-    one,
-    truthiness,
-    isEqual,
-    isGreaterThan,
-    isLessThanOrEqualTo,
-    isOdd,
-    isEven,
-    isSquare,
-    startsWith,
-    containsVowels,
-    isLowerCase
+  negate,
+  both,
+  either,
+  none,
+  one,
+  truthiness,
+  isEqual,
+  isGreaterThan,
+  isLessThanOrEqualTo,
+  isOdd,
+  isEven,
+  isSquare,
+  startsWith,
+  containsVowels,
+  isLowerCase,
 };
